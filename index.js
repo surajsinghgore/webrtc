5e0c62417427b2fecf5e5b25fd63c3b304268071
suraj singh rajput <surajthakurrs45@gmail.com>
2024-08-31 08:43:24 +0530

    web testing

--- index.js
+++ index.js
@@ -1,78 +1,32 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static('public'));
 
-const rooms = {};
+io.on('connection', socket => {
+    console.log('New client connected');
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-
-  socket.on('create-room', () => {
-    const roomId = uuidv4();
-    rooms[roomId] = { presenter: socket.id, viewers: [] };
-    socket.join(roomId);
-    socket.emit('room-created', roomId);
-    console.log(`Room created with ID: ${roomId}`);
-  });
-
-  socket.on('join-room', (roomId) => {
-    if (rooms[roomId]) {
-      rooms[roomId].viewers.push(socket.id);
-      socket.join(roomId);
-      io.to(roomId).emit('new-viewer');
-    }
-  });
-
-  socket.on('offer', (data) => {
-    const room = rooms[data.roomId];
-    if (room) {
-      socket.to(data.roomId).emit('offer', data.offer);
-    }
-  });
-
-  socket.on('answer', (data) => {
-    const room = rooms[data.roomId];
-    if (room) {
-      socket.to(room.presenter).emit('answer', data.answer);
-    }
-  });
-
-  socket.on('ice-candidate', (data) => {
-    const room = rooms[data.roomId];
-    if (room) {
-      socket.to(data.target).emit('ice-candidate', data.candidate);
-    }
-  });
-
-  socket.on('chat-message', (message) => {
-    io.emit('chat-message', message);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-    Object.entries(rooms).forEach(([roomId, room]) => {
-      if (room.presenter === socket.id) {
-        io.to(roomId).emit('room-closed');
-        delete rooms[roomId];
-      } else {
-        room.viewers = room.viewers.filter(id => id !== socket.id);
-        if (room.viewers.length === 0) {
-          io.to(room.presenter).emit('room-closed');
-        }
-      }
+    socket.on('offer', offer => {
+        socket.broadcast.emit('offer', offer);
     });
-  });
+
+    socket.on('answer', answer => {
+        socket.broadcast.emit('answer', answer);
+    });
+
+    socket.on('ice-candidate', candidate => {
+        socket.broadcast.emit('ice-candidate', candidate);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
